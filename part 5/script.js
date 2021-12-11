9484d804d9d8c30e4ef3e042225ac592d3d834a9
FIRS <shuliak.lyubov@gmail.com>
2021-12-11 23:47:11 +0200

    hff

--- part 5/script.js
+++ part 5/script.js
@@ -9,7 +9,7 @@ mainArr.forEach((e) => {
 });
 
 let counter = [];
-let finalArr = fd
+let finalArrHorizontal = fd
   .map((e) => Object.values(e))
   .filter((el, id, aarr) => aarr[id][0][0] === aarr[id][1][0])
   .sort((a, b) => {
@@ -20,15 +20,37 @@ let finalArr = fd
   });
 
 let fg = [];
-finalArr.forEach((eld) => {
+finalArrHorizontal.forEach((eld) => {
   let arr = [eld[0][1], eld[1][1]].sort((a, b) => a - b);
   let fd = [];
   for (let k = arr[0]; k <= arr[1]; k++) {
-    fd.push(k);
+    fd.push({ [k]: 1 });
   }
   fg.push({ [eld[0][0]]: [...fd] });
 });
+let count = [];
+
+let finalArrVertical = fd
+  .map((e) => Object.values(e))
+  .filter((el, id, aarr) => aarr[id][0][1] === aarr[id][1][1])
+  .sort((a, b) => {
+    if (a[0][0] === b[0][0]) {
+        count = count.concat([a].concat(b));
+    }
+    return Math.max(a[1][0], a[0][0]) - Math.max(b[0][0], b[1][1]);
+  });
+
+let jh = [];
+finalArrVertical.forEach((eld) => {
+  let arr = [eld[0][0], eld[1][0]].sort((a, b) => a - b);
+  let fd = [];
+  for (let k = arr[0]; k <= arr[1]; k++) {
+    fd.push({ [k]: 1 });
+  }
+  jh.push({ [eld[0][1]]: [...fd] });
+});
 
 console.log(fg);
-console.log(counter);
-console.log(finalArr);
+
+console.log(jh);
+console.log(count);
